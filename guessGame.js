fd8e3598a6750b64883285a01d193fe81b747ac8
Tom Cieslukowski <44185784+ciesluk@users.noreply.github.com>
2022-07-20 18:40:33 -0400

    Updated the game
    Update the guessing game with a counter, and letting the user know if the number is higher or lower. Additionally, added a max number that the user can play up too, no longer limiting the user to numbers from 1 - 10.

--- guessGame.js
+++ guessGame.js
@@ -1,12 +1,24 @@
+let maxNum = parseInt(prompt("Please enter a max number for the max of the random number"));
+
+while(!maxNum){
+    maxNum = parseInt(prompt("Please enter a max number for the max of the random number"));
+}
 //Generate a random number from 1 to 10
-let randomNumber = Math.floor(Math.random() * 10 + 1);
+let randomNumber = Math.floor(Math.random() * maxNum + 1);
 
 //Ask the user for their first guess
 let guess = prompt("Guess a random number from 1 - 10 ");
 
+let counter = 1;
 //Check to see if the random number is equal to the guess, if not prompt again
 while(guess != randomNumber){
-    guess = prompt("Try again, guess a random number from 1 - 10 ");
+    counter++; 
+    //Checking if the guess is lower or higher than the random number
+    if(guess < randomNumber){
+        guess = prompt("Try again, guess too low, guess a random number from 1 - 10 ");
+    }else if(guess > randomNumber){
+        guess = prompt("Try again, guess too high, guess a random number from 1 - 10 ");
+    }
 }
 
 //Congragulate the user on getting the random number
